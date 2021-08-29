a74dde5a51b235f484b6475715a5288d89635432
Patrick <patrick.e.muller@gmail.com>
2021-08-29 18:33:49 -0300

    Add very basic JSON endpoint for home and posts#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@
 class PagesController < ApplicationController
   def homepage
     @posts = Post.all.order(created_at: :desc)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,10 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find_by(slug: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @post }
+    end
   end
 end
