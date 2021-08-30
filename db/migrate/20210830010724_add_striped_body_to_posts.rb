1b0a206afe9a5d29341ce4498ad503dbf6b58b2e
Patrick <patrick.e.muller@gmail.com>
2021-08-29 22:18:52 -0300

    Add default value for striped_body of Post model

--- db/migrate/20210830010724_add_striped_body_to_posts.rb
+++ db/migrate/20210830010724_add_striped_body_to_posts.rb
@@ -1,5 +1,5 @@
 class AddStripedBodyToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_column :posts, :striped_body, :text, null: false
+    add_column :posts, :striped_body, :text, null: false, default: ''
   end
 end
